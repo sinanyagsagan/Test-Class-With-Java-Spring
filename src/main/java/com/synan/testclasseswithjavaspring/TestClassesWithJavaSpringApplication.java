ef3a91a53d5e359b52af419355a72b169b212a80
sinanyagsagan <s.yagsagan.sy@gmail.com>
2023-11-09 01:35:42 +0300

    spring boot project files created

---
+++ src/main/java/com/synan/testclasseswithjavaspring/TestClassesWithJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.synan.testclasseswithjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClassesWithJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestClassesWithJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/synan/testclasseswithjavaspring/TestClassesWithJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synan.testclasseswithjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestClassesWithJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
